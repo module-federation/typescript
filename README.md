514a023281a878b2455aad80aa3092263698718f
Prasanna Mestha <prasanna.mestha@practiskills.com>
2022-04-15 20:12:11 +0530

    download types into the host app

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
-> **WIP**
+## Typescript support for module federated apps
 
-## Typescript support for federated apps
+A webpack plugin for sharing typescript types between module federated apps.
 
-**What needs to be done:**
+### Installation
+```
+$ npm config set @module-federation:registry https://r.privjs.com
+$ npm i @module-federation/typescript
+```
 
-[x] Create webpack plugin to create types of the files exposed by the remote under `dist/@federated-types`
+### Usage
+Register the plugin in webpack.config.js file
+```
+const FederatedTypesPlugin = require('./webpack/tsplugin')
 
-[ ] In the host app enable consuming the above generated types
+const federationConfig = {
+  name: 'my-app',
+  filename: 'remoteEntry.js',
+  exposes: {
+    './Button': './src/Button',
+    './Input': './src/Input',
+  },
+  shared: ['react', 'react-dom'],
+}
+
+plugins: [
+  // ...
+  new ModuleFederationPlugin(federationConfig),
+  new FederatedTypesPlugin(federationConfig),
+]
+```
+
+You need to register this plugin in both remote and host apps. The plugin will automatically create a directory named `@mf-typescript` in the host app - that contains all the types exported by the remote apps.
+
+In your file:
+```
+import RemoteButtonType from "../@mf-typescript/Button";
+
+const RemoteButton = React.lazy(
+  () => import("app2/Button")
+) as unknown as typeof RemoteButtonType;
+```
+
+
+### What's next?
+[ ] Refetch types on save
--- src/index.js
+++ src/index.js
@@ -1,27 +1,59 @@
 const ts = require("typescript");
-
+const download = require('download');
+const fs = require('fs')
+const axios = require('axios')
 module.exports = class FederatedTypesPlugin {
   constructor(options) {
     this.options = options;
   }
   apply(compiler) {
     const run = () => {
-      // 1. Get the `exposes` value from federationConfig
-      // 2. Create .d.ts files in the build directory
-      // 3. [TODO] Import the types from `remotes` value from federationConfig
-      const fileNames = Object.values(this.options.exposes);
+      const exposedComponents = this.options.exposes
+      const remoteComponents = this.options.remotes
 
-      const program = ts.createProgram(fileNames, {
-        declaration: true,
-        emitDeclarationOnly: true,
-        outDir: "./dist/@federated-types", // replace this with build directory
-      });
+      if (exposedComponents) {
+        const fileNames = Object.values(this.options.exposes);
+        const typeFiles = fileNames.map(f => {
+          const split = f.split('/')
+          return split[split.length - 1] + '.d.ts'
+        })
 
-      program.emit();
-      console.log("Types ready for export");
+        fs.writeFile('./dist/@mf-typescript/__types_index.json', JSON.stringify(typeFiles), (e) => {
+          if (e) {
+            console.log('Error saving the types index')
+          }
+        })
+        const program = ts.createProgram(fileNames, {
+          declaration: true,
+          emitDeclarationOnly: true,
+          outDir: "./dist/@mf-typescript/", // replace this with build directory
+        });
+
+        program.emit();
+      }
+
+      // Time to import the remote types
+      if (remoteComponents) {
+        // Get the remote URL origin
+        const remoteUrls = Object.values(remoteComponents).map(r => {
+          const url = new URL(r.split('@')[1])
+          return url.origin
+        })
+
+        remoteUrls.forEach(remote => {
+          axios.get(`${remote}/@mf-typescript/__types_index.json`)
+            .then(indexFileResp => {
+              // Download all the d.ts files mentioned in the index file
+              indexFileResp.data?.forEach(file => download(`${remote}/@mf-typescript/${file}`, '@mf-typescript'))
+            })
+            .catch(e => console.log(e))
+
+        })
+      }
+
     };
 
-    compiler.hooks.afterCompile.tap("NextFederation", (compilation) => {
+    compiler.hooks.afterCompile.tap("FederatedTypes", (compilation) => {
       run(compilation);
     });
   }
